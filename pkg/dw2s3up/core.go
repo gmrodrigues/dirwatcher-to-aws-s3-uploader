5b0ec42dd4480c8409570c2322b44ef2d7e7bf51
Glauber Machado Rodrigues <glauber.rodrigues@F003021.finnet.corp>
2019-01-02 19:30:40 -0200

    more elaborated watch interface

--- pkg/dw2s3up/core.go
+++ pkg/dw2s3up/core.go
@@ -10,12 +10,34 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
+const (
+	CREATE = "CREATE"
+	WRITE  = "WRITE"
+	REMOVE = "REMOVE"
+	RENAME = "RENAME"
+	CHMOD  = "CHMOD"
+	PUSHED = "PUSHED"
+)
+
+var fsNotfyOpToStr = map[fsnotify.Op]string{
+	fsnotify.Create: CREATE,
+	fsnotify.Write:  WRITE,
+	fsnotify.Remove: REMOVE,
+	fsnotify.Rename: RENAME,
+	fsnotify.Chmod:  CHMOD,
+}
+
+type File struct {
+	Name string
+	Meta interface{}
+}
+
 type WatcherEvent struct {
-	Filename string
-	Type     string //  Create, Write, Remove, Rename,  Chmod
-	Stat     Stat
-	Watcher  *Watcher
-	Meta     interface{}
+	File    *File
+	Type    string //  Create, Write, Remove, Rename,  Chmod, Pushed
+	Stat    FileInfo
+	Watcher *Watcher
+	Meta    interface{}
 }
 
 type Watcher struct {
@@ -23,6 +45,7 @@ type Watcher struct {
 	watcher *fsnotify.Watcher
 	done    chan bool
 	subs    []chan WatcherEvent
+	pushed  chan File
 	metaMap map[string]interface{}
 }
 
@@ -32,6 +55,7 @@ type FileInfo struct {
 	Mode    os.FileMode // file mode bits
 	ModTime time.Time   // modification time
 	IsDir   bool        // abbreviation for Mode().IsDir()
+	Exists  bool        // file exists
 	Sys     interface{} // underlying data source (can return nil)
 }
 
@@ -39,8 +63,9 @@ type Watchable interface {
 	Dirname() string
 	SubscribeChan(ch chan WatcherEvent) error
 	SubscribeFunc(f func(e WatcherEvent)) error
-	AddFile(filename string) error
-	AddFileWithMeta(filename string, meta interface{}) error
+	AddFile(file File) error
+	ForcePushFile(file File) error
+	ForgetFile(filename string) (file File, err error)
 	Start() error
 	Done() error
 }
@@ -58,6 +83,7 @@ func NewWatchable(dirName string) (wtb Watchable, err error) {
 	}
 
 	w.done = make(chan bool)
+	w.pushed = make(chan File)
 	w.subs = make([]chan WatcherEvent, 1)
 	w.metaMap = make(map[string]interface{})
 
@@ -77,8 +103,50 @@ func (w *Watcher) Done() error {
 
 func (w *Watcher) Start() error {
 	defer w.watcher.Close()
-	matches, _ := filepath.Glob(fmt.Sprintf("%s/*", w.Dirname()))
-	log.Printf("Glob matches %s: %#v", w.Dirname(), matches)
+	dirGlobs, _ := filepath.Glob(fmt.Sprintf("%s/*", w.Dirname()))
+	log.Printf("Glob matches %s: %#v", w.Dirname(), dirGlobs)
+
+	for _, f := range dirGlobs {
+		w.pushed <- File{Name: f}
+	}
+
+	handle := func(file File, op string) (WatcherEvent, error) {
+		if file.Meta == nil {
+			file.Meta = w.metaMap[file.Name]
+		}
+		stat, stat_err := os.Stat(file.Name)
+		exists := !os.IsNotExist(stat_err)
+		s := FileInfo{
+			Name:   filepath.Base(file.Name),
+			Exists: exists,
+		}
+		if exists {
+			s = FileInfo{
+				Name:    stat.Name(),    // base name of the file
+				Size:    stat.Size(),    // length in bytes for regular files; system-dependent for others
+				Mode:    stat.Mode(),    // file mode bits
+				ModTime: stat.ModTime(), // modification time
+				IsDir:   stat.IsDir(),   // abbreviation for Mode().IsDir()
+				Exists:  exists,         // file exists
+				Sys:     stat.Sys(),     // underlying data source (can return nil)
+			}
+		}
+		e := WatcherEvent{
+			File:    &file,
+			Type:    op,
+			Watcher: w,
+			Stat:    s,
+		}
+		e.tick()
+
+		for _, sub := range w.subs {
+			go func() {
+				sub <- e
+			}()
+		}
+
+		return e, nil
+	}
 
 	for {
 		select {
@@ -86,29 +154,8 @@ func (w *Watcher) Start() error {
 			return nil
 		case event, ok := <-w.watcher.Events:
 			if ok {
-				stat, _ = os.Stat(event.Name)
-				e := WatcherEvent{
-					Filename: event.Name,
-					Type:     event.Op.String(),
-					Watcher:  w,
-					Meta:     w.metaMap[event.Name],
-					Stat: Stat{
-						Name    stat.Name(),      // base name of the file
-						Size    stat.Size(),       // length in bytes for regular files; system-dependent for others
-						Mode    os.FileMode // file mode bits
-						ModTime time.Time   // modification time
-						IsDir   bool        // abbreviation for Mode().IsDir()
-						Sys     interface{} // underlying data source (can return nil)
-					}
-				}
-				
-				log.Printf("event: %#v %#v\n", e, event)
-
-				for _, sub := range w.subs {
-					go func() {
-						sub <- e
-					}()
-				}
+				e, _ := handle(File{Name: event.Name}, fsNotfyOpToStr[event.Op])
+				log.Printf("event: %#v %#v \n", e, event)
 			}
 		case err, ok := <-w.watcher.Errors:
 			if !ok {
@@ -116,25 +163,48 @@ func (w *Watcher) Start() error {
 					log.Printf("Err: %s\n", err.Error())
 				}
 			}
+		case file := <-w.pushed:
+			e, _ := handle(file, PUSHED)
+			log.Printf("event: %#v %#v \n", e, event)
 		case <-time.After(time.Minute):
 			log.Printf("Timout\n")
 		}
 	}
 }
 
-func (w *Watcher) AddFileWithMeta(filename string, meta interface{}) (err error) {
-	if w.metaMap[filename] == nil {
-		err = w.watcher.Add(filename)
+func (w *Watcher) AddFile(file File) (err error) {
+	log.Printf("AddFileWithMeta: %s", file.Name)
+	if w.metaMap[file.Name] == nil {
+		err = w.watcher.Add(file.Name)
 		if err != nil {
-			w.metaMap[filename] = meta
+			if file.Meta != nil {
+				w.metaMap[file.Name] = file.Meta
+			} else {
+				w.metaMap[file.Name] = true
+			}
 		}
 		return err
 	}
 	return nil
 }
 
-func (w *Watcher) AddFile(filename string) (err error) {
-	return w.AddFileWithMeta(filename, true)
+func (w *Watcher) ForcePushFile(file File) (err error) {
+	w.pushed <- file
+	return nil
+}
+
+func (w *Watcher) ForgetFile(filename string) (file File, err error) {
+	log.Printf("ForgetFile: %s", filename)
+	err = w.watcher.Remove(filename)
+	meta := w.metaMap[filename]
+	if meta != nil {
+		delete(w.metaMap, filename)
+	}
+	return File{Name: filename, Meta: meta}, err
+}
+
+func (w *Watcher) contains(filename string) bool {
+	return w.metaMap[filename] != nil
 }
 
 func (w *Watcher) Dirname() string {
@@ -174,21 +244,34 @@ func (w *Watcher) SubscribeFunc(f func(e WatcherEvent)) error {
 
 // Create, Write, Remove, Rename,  Chmod
 func (we *WatcherEvent) isCreate() bool {
-	return we.Type == "Create"
+	return we.Type == CREATE
 }
 
 func (we *WatcherEvent) isWrite() bool {
-	return we.Type == "Write"
+	return we.Type == WRITE
 }
 
 func (we *WatcherEvent) isRemove() bool {
-	return we.Type == "Remove"
+	return we.Type == REMOVE
 }
 
 func (we *WatcherEvent) isRename() bool {
-	return we.Type == "Rename"
+	return we.Type == RENAME
 }
 
 func (we *WatcherEvent) isChmod() bool {
-	return we.Type == "Chmod"
+	return we.Type == CHMOD
+}
+
+func (we *WatcherEvent) isPushed() bool {
+	return we.Type == PUSHED
+}
+
+func (we *WatcherEvent) tick() bool {
+	if we.isCreate() {
+		we.Watcher.AddFile(*we.File)
+	} else if !we.Stat.Exists {
+		we.Watcher.ForgetFile(we.File.Name)
+	}
+	return true
 }
